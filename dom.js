9dcb884e14510515a29612bd5d30a25c28f34d8c
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-06-03 08:47:36 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -27,3 +27,13 @@ var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.backgroundColor = 'green'; 
 var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
 thirdItem.style.backgroundColor = 'white'; 
+
+// QUERY SELECTORALL
+var secondListitem = document.querySelectorAll('.list-group-item:nth-child(2)');
+secondListitem.style.color = 'green';
+
+var odd = document.querySelectorAll('.li:nth-child(odd)');
+for(var i = 0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+}
+
