cb8ac4ad96227930bc7e4bb46de7d5dbf85e3653
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-06-03 08:32:59 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -11,3 +11,19 @@ for(var i=0; i<li.length; i++){
 for(var i=0; i<li.length; i++){
     li[i].style.fontWeight = 'bold';
 }
+
+// QUERY SELECTOR
+var header = document.querySelector('main-header');
+header.style.borderBottom = 'solid 6px #ccc';
+
+
+var input = document.querySelector('input');
+input.value = 'Hello there';
+
+var submit = document.querySelector('input[type = "submit"]');
+submit.value = 'Send';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green'; 
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.backgroundColor = 'white'; 
