f2020d014ce58931ba49a652a45f24fe62ae6a4c
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-06-04 07:55:24 +0530

    Update traversing_dom.js

--- traversing_dom.js
+++ traversing_dom.js
@@ -31,3 +31,29 @@ console.log(itemList.previousSibling);
 
 // previousElementSibling
 console.log(itemList.previousElementSibling);
+
+// createElement
+var newDiv = document.createElement('div');
+
+// add class name
+newDiv.className = 'hello';
+
+// add id
+newDiv.id = 'hello1';
+
+// set attribute
+newDiv.setAttribute('title', 'hello div');
+
+// create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+container.insertBefore(newDiv,h1);
+
