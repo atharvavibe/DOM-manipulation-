8f2197321120a4ee0b1a665d95561825475029e5
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-06-11 17:26:37 +0530

    Update main.js

--- booking_appointment_app/main.js
+++ booking_appointment_app/main.js
@@ -2,9 +2,16 @@ var form = document.getElementById('my-form');
 var info = document.getElementById('name');
 var email = document.getElementById('email');
 
+let obj = {
+    name : info,
+    emailId : email
+};
+
+let myObj = JSON.stringify(obj);
+
 form.addEventListener('submit', store);
 
 function store(e){
     e.preventDefault();
-    localStorage.setItem(info, email);
+    localStorage.setItem("myObj", myObj);
 }
